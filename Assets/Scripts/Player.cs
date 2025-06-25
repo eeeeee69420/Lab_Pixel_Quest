dd6e7c44c50ec53de8b9f62ae164c3886da9c00b
eeeeee69420 <128625940+eeeeee69420@users.noreply.github.com>
2025-06-24 22:07:53 -0400

    start

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public float velocityH;
+    public float velocityV;
+    public Rigidbody2D playerBody;
+    public SpriteRenderer playerSprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerBody = GetComponent<Rigidbody2D>();
+        playerSprite = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+        Vector2 inputDirection = new Vector2(inputX, inputY);
+        if (inputDirection.magnitude > 1)
+            inputDirection = inputDirection.normalized;
+        if (inputDirection.x < 0)
+        {
+            playerSprite.flipX = true;
+        }
+        else if (inputDirection.x > 0)
+        {
+            playerSprite.flipX = false;
+        }
+        playerBody.position += speed * Time.deltaTime * inputDirection;
+    }
+}
