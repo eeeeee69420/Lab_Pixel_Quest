727457ecf4d2267b3e9f68bd262c0c19a8785a8b
eeeeee69420 <128625940+eeeeee69420@users.noreply.github.com>
2025-06-25 22:49:56 -0400

    start on animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,11 +8,13 @@ public class Player : MonoBehaviour
     public float velocityV;
     public Rigidbody2D playerBody;
     public SpriteRenderer playerSprite;
+    public PlayerAnimator playerAnimator;
     // Start is called before the first frame update
     void Start()
     {
         playerBody = GetComponent<Rigidbody2D>();
         playerSprite = GetComponentInChildren<SpriteRenderer>();
+        playerAnimator = GetComponent<PlayerAnimator>();
     }
 
     // Update is called once per frame
@@ -22,7 +24,9 @@ public class Player : MonoBehaviour
         float inputY = Input.GetAxis("Vertical");
         Vector2 inputDirection = new Vector2(inputX, inputY);
         if (inputDirection.magnitude > 1)
+        {
             inputDirection = inputDirection.normalized;
+        }
         if (inputDirection.x < 0)
         {
             playerSprite.flipX = true;
